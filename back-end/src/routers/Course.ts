f8de8a9fdaf824824178ff02bbda1615270e4919
dimitriearose <dimitri.ea.rose@gmail.com>
2021-01-22 19:08:10 -0500

    basic api complete

--- back-end/src/routers/Course.ts
+++ back-end/src/routers/Course.ts
@@ -28,13 +28,57 @@ router.get('/course/:courseId', auth, async (req:any,res,next) => {
     try {
         const course = await Course.findById(courseId)
 
-
         if (!course) {
-           return res.status(400).send("Invalid")
+           return res.status(404).send("Invalid")
         }
 
         res.send({course})
 
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.patch('/course/:courseId', auth, async (req, res) => {
+    const {name,category,originalprice,price,details,fineprint} = req.body
+    
+    try {
+        const courseId = req.params.courseId
+        const course = await Course.findById(courseId)
+
+        if (!course) {
+            return res.status(400).send("Invalid")
+        }
+
+        course.name = name
+        course.category = category
+        course.originalprice = originalprice
+        course.price = price
+        course.details = details
+        course.fineprint = fineprint
+
+        await course.save()
+        res.send(course)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.delete('/course/:courseId', auth, async (req, res) => {
+    
+    try {
+        const courseId = req.params.courseId
+
+        const course = await Course.findById(courseId)
+
+        if (!course) {
+            return res.status(400).send("Invalid")
+        }
+
+
+        await course.remove()
+
+        res.send(course)
     } catch (error) {
         res.status(500).send(error)
     }
